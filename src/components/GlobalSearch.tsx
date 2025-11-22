8866015075267e2a6def8bcdac7f6531ad729794
barcanvladut <barcanvladi@gmail.com>
2025-11-22 14:46:52 +0100

    minor changes

--- src/components/GlobalSearch.tsx
+++ src/components/GlobalSearch.tsx
@@ -58,7 +58,7 @@ export default function GlobalSearch() {
           <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
         </svg>
         <span className="hidden md:inline">Search...</span>
-        <span className="hidden md:inline text-xs border border-neutral-700 px-1.5 rounded bg-neutral-800 text-neutral-500">⌘K</span>
+        <span className="hidden md:inline text-xs border border-neutral-700 px-1.5 rounded bg-neutral-800 text-neutral-500">Ctrl+K</span>
       </button>
     );
   }
