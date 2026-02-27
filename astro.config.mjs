7298ab6d42a84c5c254974c04f08de3c9f71e524
barcanvladut <barcanvladi@gmail.com>
2026-02-27 21:52:33 +0100

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   // This is still correct
   output: 'static',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
@@ -20,7 +23,7 @@ export default defineConfig({
       theme: 'dracula', // or your preferred code theme
     }
   },
-  integrations: [mdx(),svelte()],
+  integrations: [react(), mdx(),svelte()],
   image: {
     domains: ['live.staticflickr.com'],
   }
